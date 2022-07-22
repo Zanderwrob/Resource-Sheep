adb3238214e47a63ae8879f48832573fffc9a1d4
Zanderwrob <zanderwrob@gmail.com>
2022-07-22 08:10:08 -0500

    Adding more folders, and added texure for iron_sheep_fur

--- src/main/java/com/zanderwrob/resourceSheep/ResourceSheep.java
+++ src/main/java/com/zanderwrob/resourceSheep/ResourceSheep.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.zanderwrob.resourceSheep;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.world.level.block.Block;
@@ -18,13 +18,14 @@ import org.slf4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
+@Mod(ResourceSheep.MOD_ID)
+public class ResourceSheep
 {
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
+    public static final String MOD_ID = "resourceSheep";
 
-    public ExampleMod()
+    public ResourceSheep()
     {
         // Register the setup method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
