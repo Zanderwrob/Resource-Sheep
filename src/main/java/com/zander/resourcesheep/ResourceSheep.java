18e1881c5764ad984194f071da136ec9ae39e435
zanderWroblewski <zanderwrob@gmail.com>
2022-12-06 11:24:46 -0600

    Adding Item registry, and initial first example item.

--- src/main/java/com/zander/resourcesheep/ResourceSheep.java
+++ src/main/java/com/zander/resourcesheep/ResourceSheep.java
@@ -1,12 +1,16 @@
 package com.zander.resourcesheep;
 
+import com.zander.resourcesheep.init.ItemInit;
 import net.minecraft.world.entity.animal.Sheep;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import oshi.hardware.common.AbstractUsbDevice;
 
 @Mod("resourcesheep")
 public class ResourceSheep {
@@ -17,10 +21,15 @@ public class ResourceSheep {
         @Override
         @OnlyIn(Dist.CLIENT)
         public ItemStack makeIcon() {
-            return null;
+
+            return new ItemStack(ItemInit.EXAMPLE_ITEM.get());
         }
     };
     public ResourceSheep() {
+        IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ItemInit.ITEMS.register(bus);
+
         MinecraftForge.EVENT_BUS.register(this);
     }
 }
--- src/main/java/com/zander/resourcesheep/init/ItemInit.java
+++ src/main/java/com/zander/resourcesheep/init/ItemInit.java
@@ -11,6 +11,10 @@ import java.util.function.Supplier;
 public class ItemInit {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ResourceSheep.MOD_ID);
 
+    public static final RegistryObject<Item> EXAMPLE_ITEM = register("example item", () -> new Item(new Item.Properties()
+// Example Item's Properties
+            .tab(ResourceSheep.RESOURCESHEEP_TAB)
+    ));
     public static <T extends Item> RegistryObject<T> register(final String name, final Supplier<T> item) {
         return ITEMS.register(name, item);
     }
